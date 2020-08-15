d717a7f257c4a0d8f9e3d948c65801b2c85e5b30
Alfredo Arcia <arcia86alfredo@gmail.com>
2020-08-15 16:07:01 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # TIG-Stack-Docker
 TIG stack on Docker for network monitoring of Cisco SDWAN controller
+
+## Getting Started
+
+I ran this code using a Mac-OS. with docker already installed. the main goal of this is running a monitoring completed system to collect information from the vManager using 
+RESTAPIService and save those values into a InfluxDB database then you can use Grafana dashboard to monitoring.
+
+### Prerequisites
+
+Docker, Cisco Sandbox vManager reserved.  
+
+## Create your docker enviroment
+
+Firts create your network environment and volume for the Grafana and influxdb container.
+
+```
+docker network create monitoring
+docker volume create grafana-volume
+docker volume create influxdb-volume
+docker network ls
+docker volume ls
+```
+You can also need to prepare the influxdb environment.
+
+```
+docker run --rm \
+  -e INFLUXDB_DB=telegraf -e INFLUXDB_ADMIN_ENABLED=true \
+  -e INFLUXDB_ADMIN_USER='MyUser' \
+  -e INFLUXDB_ADMIN_PASSWORD='Mypass' \
+  -e INFLUXDB_USER=telegraf -e INFLUXDB_USER_PASSWORD='Mypass' \
+  -v influxdb-volume:/var/lib/influxdb \
+  influxdb /init-influxdb.sh
+```
+just start your containers.
+```
+docker-compose up -d
+```
+### verifying container is running
+
+Once is running you can verify the container is running:
+```
+docker ps
+```
+Open browser to verify Grafana is running localhost:3000
+
+### Collect data from the vManager
+
+Create your virtual environment and install your requirements
+
+```
+virtualenv venv --python=python3
+source venv/bin/activate
+pip install -r requirements.txt
+```
+Edit the vmanage_login.yaml file with your credentials
+```
+vim vmanage_login.yaml
+
+# vManage Connectivity Info
+vmanage_host: 'YOUR_IP'
+vmanage_port: 8443
+vmanage_username: admin
+vmanage_password: admin
+hub_routers:
+  - system_ip: 'YOUR_system_ip'
+```
+Run python scripts.
+```
+./approute_statistics_api.py
+./device-health.py
+```
+![Imagen of localhost:3000](/imagen/Capt1.png)
+
---
+++ device-health.py
@@ -0,0 +1,120 @@
+from influxdb import InfluxDBClient
+import requests
+import sys
+import json
+import time
+import yaml
+
+requests.packages.urllib3.disable_warnings()
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+class Authentication:
+
+    @staticmethod
+    def get_jsessionid(vmanage_host, vmanage_port, username, password):
+        api = "/j_security_check"
+        base_url = "https://%s:%s"%(vmanage_host, vmanage_port)
+        url = base_url + api
+        payload = {'j_username' : username, 'j_password' : password}
+        
+        response = requests.post(url=url, data=payload, verify=False)
+        try:
+            cookies = response.headers["Set-Cookie"]
+            jsessionid = cookies.split(";")
+            return(jsessionid[0])
+        except:
+            print("\nNo valid JSESSION ID returned")
+            exit()
+       
+    @staticmethod
+    def get_token(vmanage_host, vmanage_port, jsessionid):
+        headers = {'Cookie': jsessionid}
+        base_url = "https://%s:%s"%(vmanage_host, vmanage_port)
+        api = "/dataservice/client/token"
+        url = base_url + api      
+        response = requests.get(url=url, headers=headers, verify=False)
+        if response.status_code == 200:
+            return(response.text)
+        else:
+            return None
+
+if __name__ == '__main__':
+
+    try:
+
+        with open("vmanage_login.yaml") as f:
+            config = yaml.safe_load(f.read())
+
+        vmanage_host = config["vmanage_host"]
+        vmanage_port = config["vmanage_port"]
+        username = config["vmanage_username"]
+        password = config["vmanage_password"]  
+
+        Auth = Authentication()
+        jsessionid = Auth.get_jsessionid(vmanage_host,vmanage_port,username,password)
+        token = Auth.get_token(vmanage_host,vmanage_port,jsessionid)
+
+        if token is not None:
+            headers = {'Content-Type': "application/json",'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
+        else:
+            headers = {'Content-Type': "application/json",'Cookie': jsessionid}
+
+        base_url = "https://%s:%s/dataservice"%(vmanage_host,vmanage_port)     
+
+        # Get app route statistics for tunnels between Hub routers and Spoke routers.
+        status = []
+        total_records = 0
+        payload = {}
+
+        api_url = "/device/system/status?deviceId=1.1.1.11"
+
+        url = base_url + api_url
+
+        response = requests.request("GET", url=url, headers=headers, data = payload, verify=False)
+        cpu_stats = response.json()["data"]
+
+        # loop over the API response variable items and create records to be stored in InfluxDB
+
+        if response.status_code == 200:
+            cpu_stats = response.json()["data"]
+
+            for item in cpu_stats:
+                temp = {
+                        "measurement": "CPU_stats",
+                        "tags": {
+                                    "Device": item['vdevice-name']
+                                },
+                        "fields": {
+                                    "CPU": float(item['cpu_system']),
+                                    "Memoria_free": float(item['mem_free']),
+                                    "disk_avail": float(item['disk_avail'].replace('M','')),
+                                    "lastupdated": float(item['lastupdated'])
+                                    }
+                        }
+
+                status.append(temp)
+                print(status)
+                total_records = total_records+1
+    
+        else:
+            print("Failed to retrieve app route statistics\n")
+
+        # login credentials for InfluxDB
+
+        USER = 'MyUser'
+        PASSWORD = 'MyPass'
+        DBNAME = 'telegraf'
+        host='localhost'
+        port=8086
+
+        client = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
+
+        client.write_points(status)
+        time.sleep(2)
+
+        print("Stored %s records in influxdb"%total_records)
+
+
+    except Exception as e:
+        print('Exception line number: {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
